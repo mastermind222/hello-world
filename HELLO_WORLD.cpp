705a998ffde13291dbab02afd64c01f5beae6da7
theneek14 <neel.19je0558@mc.iitism.ac.in>
2020-10-03 17:11:54 +0530

    Create HELLO_WORLD.cpp
    PR for Hacktoberfest'20

---
+++ HELLO_WORLD.cpp
@@ -0,0 +1,16 @@
+# HELLO_WORLD.CPP
+
+**Description :** 
+This C++ code prints Hello World !
+
+```cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+        cout<<"Hello World!";
+        return 0;
+}
+```
+
+**End**
